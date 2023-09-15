878c65e1d250d6a33a1238fec444f395e6068630
AlokKumarRai <alok20530@iiitkalyani.ac.in>
2023-09-15 17:32:14 +0530

    changed for production

--- backend/index.js
+++ backend/index.js
@@ -35,35 +35,32 @@ app.use(
   })
 );
 
-//----------------Deployment--------------
-
-const __dirname1 = path.resolve();
-
-// if (process.env.NODE_ENV==='production') {
-//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
-
-
-//----------------------------------------
-
-
-
-
 app.use(express.json());
-
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/comments", commentRoute);
 app.use("/api/search", searchRoute);
+
+
+//----------------Deployment--------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV==='production') {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+
+//----------------------------------------
 
 app.listen(8000, () => {
   console.log("Server is running!");
