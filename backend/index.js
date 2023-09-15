30c138ba68c1085f265c25c4bdb948da1d5ad5ba
AlokKumarRai <alok20530@iiitkalyani.ac.in>
2023-09-15 17:39:03 +0530

    changed port

--- backend/index.js
+++ backend/index.js
@@ -61,7 +61,8 @@ if (process.env.NODE_ENV==='production') {
 
 
 //----------------------------------------
+const PORT = process.env.PORT  || 8000
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
   console.log("Server is running!");
 });
